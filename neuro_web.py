b9e0494a1bb57e2735fcf1b0f6b8fdaa20538e96
Neuro2GIT <gruponeuroscience@gmail.com>
2025-01-30 14:46:20 -0500

    Update neuro_web.py

--- neuro_web.py
+++ neuro_web.py
@@ -8,8 +8,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaFileUpload
 
 st.set_page_config(
-    page_title="Servidor neurociência",
-    page_icon="🐭",
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -20,7 +20,7 @@ st.set_page_config(
 )
 
 # Definir as permissões e escopo de acesso
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.metadata.readonly']
 
 # Função para autenticar no Google Drive
 def authenticate():
@@ -43,6 +43,17 @@ def authenticate():
     service = build('drive', 'v3', credentials=creds)
     return service
 
+# Função para listar arquivos e pastas do Google Drive
+def list_files(service, folder_id='root'):
+    """Lista arquivos e pastas do Google Drive, dados o folder_id"""
+    results = service.files().list(
+        q=f"'{folder_id}' in parents and trashed = false",
+        fields="nextPageToken, files(id, name, mimeType)",
+        pageSize=1000
+    ).execute()
+    
+    return results.get('files', [])
+
 # Função para upload para o Google Drive
 def upload_file_to_drive(service, file, mime_type):
     """Faz o upload de um arquivo para o Google Drive."""
@@ -61,13 +72,30 @@ def main():
         st.success(st.session_state['flash_message'])
         del st.session_state['flash_message']
     
+    # Autenticar no Google Drive
+    service = authenticate()
+
+    # Barra lateral para listar pastas e arquivos
+    st.sidebar.title("Google Drive - Navegação")
+    folder_id = st.sidebar.text_input('ID da Pasta', value='root')
+
+    # Listar pastas e arquivos da pasta atual
+    files = list_files(service, folder_id)
+    
+    if files:
+        st.sidebar.write("Pastas e Arquivos:")
+        for f in files:
+            if f['mimeType'] == 'application/vnd.google-apps.folder':
+                # Exibir pastas
+                st.sidebar.write(f"📂 {f['name']}")
+            else:
+                # Exibir arquivos
+                st.sidebar.write(f"📄 {f['name']}")
+
     # Fazer upload de arquivos
     uploaded_file = st.file_uploader("Escolha um arquivo (até 2GB)", type=['pdf', 'jpg', 'jpeg', 'png', 'txt', 'mp4', 'avi', 'mkv', 'mov', 'flv', 'wmv'])
     
     if uploaded_file is not None:
-        # Autenticar no Google Drive
-        service = authenticate()
-        
         try:
             file_id = upload_file_to_drive(service, uploaded_file, uploaded_file.type)
             st.session_state['flash_message'] = f"Arquivo '{uploaded_file.name}' enviado para o Google Drive com sucesso!"
@@ -81,7 +109,7 @@ def main():
         </footer>
     """, unsafe_allow_html=True)
 
-
 # Executar o Streamlit app
 if __name__ == "__main__":
     main()
+
