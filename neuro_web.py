c7fd6f81ec288842b8793163c8439af55d404472
Neuro2GIT <gruponeuroscience@gmail.com>
2025-01-31 13:52:31 -0500

    Update neuro_web.py

--- neuro_web.py
+++ neuro_web.py
@@ -92,7 +92,7 @@ def main():
     st.title("🐁Grupo neuroscience")
 
     uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+    if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
