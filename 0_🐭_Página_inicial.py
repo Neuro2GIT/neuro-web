2dff2d3560e3560790e48e5c0866ed56c7c4fbae
Neuro2GIT <gruponeuroscience@gmail.com>
2025-02-23 23:53:37 -0500

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -126,7 +126,7 @@ dois = [
 
 # Função principal para exibir o conteúdo
 def main():
-    st.title("🧠Neuroscience interest group")
+    st.title("Neuroscience interest group")
     
     # Saudação baseada na hora do dia
     greeting = get_greeting()
