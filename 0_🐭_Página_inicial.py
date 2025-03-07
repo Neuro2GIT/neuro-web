ee389c1cc109d03cbf934eebfed6d067c63da1d1
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-07 00:24:06 -0500

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -22,11 +22,6 @@ st.set_page_config(
     menu_items={})
 st.set_option('client.showErrorDetails', True)
 
-import firebase_admin
-from firebase_admin import credentials, firestore
-import streamlit as st
-import streamlit_authenticator as stauth
-
 # Inicializa o Firebase com as credenciais armazenadas em 'st.secrets'
 if not firebase_admin._apps:
     cred = credentials.Certificate(st.secrets["firebase"])
