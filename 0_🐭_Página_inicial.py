7d38aa1a67798442c490303bcb73a7b745c5f016
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-07 01:08:06 -0500

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -16,7 +16,7 @@ st.markdown("""
         .header {
             background-color: #121212;
             padding: 10px 20px;
-            display: top;
+            display: flex;
             justify-content: space-between;
             align-items: center;
             border-bottom: 2px solid #27ae60;
