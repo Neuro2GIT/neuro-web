81aa8f665efcedafbe75c33e260ac1da1e6053ce
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-07 00:33:42 -0500

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -45,26 +45,6 @@ st.set_option('client.showErrorDetails', True)
 
 #if not check_password():
     #st.stop()  # Do not continue if check_password is not True.
-    
-# Função para gerar a saudação baseada no horário
-#def get_greeting():
-    # Definir o fuso horário do Acre (GMT-5)
-    #timezone = pytz.timezone("America/Rio_Branco")
-    
-    # Obtém a hora atual no fuso horário do Acre
-    #current_time_acre = datetime.now(timezone)
-    
-    # Extrair a hora ajustada
-    #current_hour = current_time_acre.hour
-    
-    # Definir as saudações com base na hora do dia
-    #if current_hour < 12:
-        #return "Bom dia!"
-    #elif current_hour < 18:
-        #return "Boa tarde!"
-    #else:
-        #return "Boa noite!"
-
 
 def get_doi_info(doi):
     base_url = "https://api.crossref.org/works/"
@@ -113,56 +93,6 @@ themes = {
         "10.1016/S0021-9258(18)57021-6"
     ]
 }
-
-# Função de autenticação com Google Drive
-#SCOPES = ['https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/drive.file']
-
-#def authenticate():
-    #"""Autenticação com o Google Drive usando as credenciais do Streamlit secrets"""
-    #if "google_drive_service" not in st.session_state:
-        #google_secrets = st.secrets["google"]
-        #credentials_dict = {
-            #"type": "service_account",
-            #"project_id": google_secrets["project_id"],
-            #"private_key_id": google_secrets["private_key_id"],
-            #"private_key": google_secrets["private_key"],
-            #"client_email": google_secrets["client_email"],
-            #"client_id": google_secrets["client_id"],
-            #"auth_uri": google_secrets["auth_uri"],
-            #"token_uri": google_secrets["token_uri"],
-            #"auth_provider_x509_cert_url": google_secrets["auth_provider_x509_cert_url"],
-            #"client_x509_cert_url": google_secrets["client_x509_cert_url"],
-            #"universe_domain": google_secrets["universe_domain"]
-        #}
-
-        #credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=SCOPES)
-        #service = build('drive', 'v3', credentials=credentials)
-
-        # Testa a autenticação para garantir que está funcionando corretamente
-        #test_authentication(service)
-        
-        # Armazena o serviço no session_state
-        #st.session_state["google_drive_service"] = service
-    #else:
-        #st.write("")
-
-    #return st.session_state["google_drive_service"]
-
-#def test_authentication(service):
-    #"""Função de teste para validar a autenticação"""
-    #try:
-        # Testa a autenticação
-        #about = service.about().get(fields="user").execute()
-        #st.write(f"Autenticado como: {about['user']['emailAddress']}")
-    #except Exception as e:
-        #st.error(f"Falha na autenticação: {e}")
-
-##def list_files(service, folder_id=None):
-    #"""Lista arquivos do Google Drive"""
-    #query = f"'{folder_id}' in parents" if folder_id else "trashed = false"
-    #results = service.files().list(q=query, pageSize=10, fields="files(id, name, mimeType)").execute()
-    #items = results.get('files', [])
-    #return items
 
 # Função principal para exibir o conteúdo
 def main():
