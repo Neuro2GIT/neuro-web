22f69d4ac76f23808e8000001497ab2dfec026ed
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-08 10:19:35 -0500

    Update 8_📊_Plotar gráficos.py

--- pages/8_📊_Plotar gráficos.py
+++ pages/8_📊_Plotar gráficos.py
@@ -70,11 +70,10 @@ def plotar_pesagem(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_pad
     ))
 
     fig.update_layout(
-        title="Média de Peso dos Animais (CT e DT)",
-        xaxis=dict(title="Dias", tickmode="array", tickvals=dias),
-        yaxis_title="Peso (g)",
-        legend_title="Classes",
-    )
+         title="Consumo Médio de Ração",
+    xaxis=dict(title="Dias", tickmode="array", tickvals=np.arange(1, len(medias_racao_ct) + 1)),
+    yaxis_title="Consumo (g)"
+)
 
     st.plotly_chart(fig)
     
