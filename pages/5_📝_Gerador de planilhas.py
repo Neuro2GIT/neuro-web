10b3eadf1c07b3293dbb2e683225f4c8b03587d5
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-07 22:52:25 -0500

    Update 5_📝_Gerador de planilhas.py

--- pages/5_📝_Gerador de planilhas.py
+++ pages/5_📝_Gerador de planilhas.py
@@ -76,7 +76,7 @@ def criar_planilha(num_animais_ct, num_animais_dt, num_dias):
         return b.read()
 
 # Configuração do Streamlit
-st.title("Gerar Planilha - pesagem de Animais e consumo de Ração")
+st.title("Peso dos animais e consumo de ração")
 
 # Widgets para o número de animais e dias do experimento
 num_animais_ct = st.number_input("Número de animais na classe CT:", min_value=1, value=5, step=1)
