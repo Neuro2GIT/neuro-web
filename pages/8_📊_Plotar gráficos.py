f653fd23d7a424061fe3a0ae6b3fa10b541d0bf5
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-08 09:42:04 -0500

    Update 8_📊_Plotar gráficos.py

--- pages/8_📊_Plotar gráficos.py
+++ pages/8_📊_Plotar gráficos.py
@@ -41,8 +41,8 @@ def carregar_e_processar_excel(uploaded_file):
     return {
         "medias_peso_ct": medias_peso_ct, "erro_padrao_peso_ct": erro_padrao_peso_ct, "df_ct": df_ct,
         "medias_peso_dt": medias_peso_dt, "erro_padrao_peso_dt": erro_padrao_peso_dt, "df_dt": df_dt,
-        "medias_racao_ct": medias_racao_ct, #"erro_padrao_racao_ct": erro_padrao_racao_ct,
-        "medias_racao_dt": medias_racao_dt, #"erro_padrao_racao_dt": erro_padrao_racao_dt,
+        "medias_racao_ct": medias_racao_ct,
+        "medias_racao_dt": medias_racao_dt,
         "df_racao_ct": df_racao_ct, "df_racao_dt": df_racao_dt
     }
 
@@ -81,7 +81,7 @@ def plotar_pesagem(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_pad
     st.plotly_chart(fig)
     
 # Função para plotar o gráfico de consumo de ração
-def plotar_consumo_racao(medias_racao_ct, erro_padrao_racao_ct, medias_racao_dt, erro_padrao_racao_dt):
+def plotar_consumo_racao(medias_racao_ct, erro_padrao_racao_ct, medias_racao_dt):
     fig = go.Figure()
     
     fig.add_trace(go.Scatter(
