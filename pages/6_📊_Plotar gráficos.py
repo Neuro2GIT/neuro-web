6bf24e396af0414e78ae0d7486d2328be8343291
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 15:49:31 -0500

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -70,8 +70,8 @@ def plotar_pesagem(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_pad
         #yaxis=dict(title="Peso (g)", range=[min(medias_peso_ct.min(), medias_peso_dt.min()) - 10, max(medias_peso_ct.max(), medias_peso_dt.max()) + 10]),  # Limites ajustados para o eixo Y
         margin=dict(l=50, r=50, t=50, b=50),
         legend=dict(orientation="h", x=0.5, y=-0.2, xanchor="center")
-        height=500,
-        width=700,
+        height=(500),
+        width=(700),
     )
     
     #fig.update_layout(
