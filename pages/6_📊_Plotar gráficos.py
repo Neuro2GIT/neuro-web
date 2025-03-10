81e7d59deb5fa1e9459b955473cc3359df499d2a
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 02:32:00 -0400

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -116,6 +116,8 @@ def plotar_pesagem_area(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, err
 
     fig.update_layout(
         title="Peso médio dos animais em 16 dias de experimento - erro padrão sombreado",
+        width=800,
+        height=500,
         xaxis=dict(title="Dias"),
         yaxis_title="Peso (g)",
         legend=dict(orientation="h", x=0.5, y=-0.2, xanchor="center")
