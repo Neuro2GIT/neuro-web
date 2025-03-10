0718367017c0c4c733dae681dba6cd8c888b6392
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 15:42:49 -0500

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -63,30 +63,13 @@ def plotar_pesagem(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_pad
         line=dict(color='red')
     ))
 
-    # Atualizando o layout para ajustar a escala e adicionar borda arredondada
-    fig.update_layout(
-        title="Peso médio dos animais em 16 dias de experimento",
-        xaxis=dict(
-        title="Dias", 
-        range=[dias.min(), dias.max()]  # Limite do eixo X (dias)
-        ),
-        yaxis=dict(
-        title="Peso (g)", 
-        range=[min(medias_peso_ct.min(), medias_peso_dt.min()) - 10, 
-               max(medias_peso_ct.max(), medias_peso_dt.max()) + 10]  # Limites ajustados para o eixo Y
-        ),
-        plot_bgcolor="lightgray",  # Cor de fundo do gráfico
-        margin=dict(l=50, r=50, t=50, b=50),  # Margens do gráfico
-
     # Atualizando o layout para ajustar a escala
-    #fig.update_layout(
-        #title=("Peso médio dos animais em 16 dias de experimento"),
-        #xaxis=dict(title="Dias", range=[dias.min(), dias.max()]),  # Limite do eixo X (dias)
-        #yaxis=dict(title="Peso (g)", range=[min(medias_peso_ct.min(), medias_peso_dt.min()) - 10, max(medias_peso_ct.max(), medias_peso_dt.max()) + 10]),  # Limites ajustados para o eixo Y
-        #plot_bgcolor=("lightgray"),  # Cor de fundo do gráfico
-        #paper_bgcolor=("whitesmoke"), # Cor de fundo fora do gráfico (área de papel)
-        #margin=dict(l=50, r=50, t=50, b=50),
-    #)
+    fig.update_layout(
+        title=("Peso médio dos animais em 16 dias de experimento"),
+        xaxis=dict(title="Dias", range=[dias.min(), dias.max()]),  # Limite do eixo X (dias)
+        yaxis=dict(title="Peso (g)", range=[min(medias_peso_ct.min(), medias_peso_dt.min()) - 10, max(medias_peso_ct.max(), medias_peso_dt.max()) + 10]),  # Limites ajustados para o eixo Y
+        margin=dict(l=50, r=50, t=50, b=50),
+    )
     
     #fig.update_layout(
         #title="Peso médio dos animais em 16 dias de experimento",
