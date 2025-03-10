83f50b959c2eca43a97410b805b5a9d7ba4a5c41
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 02:16:18 -0400

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -147,7 +147,7 @@ def plotar_consumo_racao(medias_racao_ct, medias_racao_dt):
 
     fig.update_layout(
         title="Consumo médio de ração em 16 dias de experimento",
-        xaxis=dict(title="Dias"),
+        xaxis=dict(title="Dias",scaleanchor="y"),
         yaxis_title="Consumo (g)",
         legend=dict(orientation="h", x=0.5, y=-0.2, xanchor="center")
     )
