2362cdadb566ef4d123bbeac55eb57844e48d5ec
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-11 13:03:49 -0400

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -350,8 +350,8 @@ with st.sidebar:
         #st.write("Selecione um arquivo excel (.xlsx) em 'Browse files' ou arraste para carregar os gráficos")    
 
 with st.container(border=True):
-        st.write("Selecione um arquivo excel (.xlsx) em 'Browse files' ou arraste para carregar os gráficos")
-        uploaded_file = st.file_uploader("Carregar o arquivo excel com os dados", type=["xlsx"])
+    st.write()
+    uploaded_file = st.file_uploader("Carregar o arquivo excel com os dados", type=["xlsx"])
 
 if uploaded_file is not None:
     # Processa os dados
