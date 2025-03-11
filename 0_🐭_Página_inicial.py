4def2cc720787ffc8b96e6b22b062b08c7d28ae2
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 21:56:10 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -86,22 +86,23 @@ def main():
     st.title("🧠 Neuroscience Interest Group")
 
     # Iterar pelos temas e artigos
-    for theme, dois in themes.items():
-        st.subheader(theme)  # Exibir o nome do tema como um subtítulo
-        st.markdown("---")  # Linha separadora para melhor organização
+    with st.container(border=True):
+        for theme, dois in themes.items():
+            st.subheader(theme)  # Exibir o nome do tema como um subtítulo
+            st.markdown("---")  # Linha separadora para melhor organização
 
-        for doi in dois:
-            title, authors, published_year, url, pdf_link = get_doi_info(doi)
+            for doi in dois:
+                title, authors, published_year, url, pdf_link = get_doi_info(doi)
             
-            if title:
-                with st.expander(title):
-                    st.markdown(f"**Autores**: {authors}")
-                    st.markdown(f"**Publicado em**: {published_year}")
-                    st.markdown(f"[Leia o artigo completo]({url})")
+                if title:
+                    with st.expander(title):
+                        st.markdown(f"**Autores**: {authors}")
+                        st.markdown(f"**Publicado em**: {published_year}")
+                        st.markdown(f"[Leia o artigo completo]({url})")
                     
-                    # Botão para baixar o PDF, se disponível
-                    if pdf_link:
-                        st.markdown(f"[Baixar PDF]({pdf_link})")
+                        # Botão para baixar o PDF, se disponível
+                        if pdf_link:
+                            st.markdown(f"[Baixar PDF]({pdf_link})")
                     
                     # Botão para marcar como lido
                     #if st.button(f"Marcar {title} como lido"):
