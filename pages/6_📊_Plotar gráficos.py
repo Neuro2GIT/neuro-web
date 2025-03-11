21a7462e91486d1b8b4be61f43ab78c9f1801ef9
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-10 21:44:37 -0400

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -347,7 +347,7 @@ if uploaded_file is not None:
     # Processa os dados
     dados = carregar_e_processar_excel(uploaded_file)
 
-    with st.container():
+    with st.container(border=True):
         # Cria as abas usando st.radio
         aba_selecionada = st.radio(
             "Escolha o tipo de gráfico",
