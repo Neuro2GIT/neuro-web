4c723c0c28d7a751339524b9d1fc0b5b97ef0645
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-12 16:24:34 -0500

    Update and rename 1_☁️_Fazer backup to 1_☁️_Fazer backup.py

--- pages/1_☁️_Fazer backup.py
+++ pages/1_☁️_Fazer backup.py
@@ -12,6 +12,33 @@ st.set_page_config(
     menu_items={})
 st.set_option('client.showErrorDetails', True)
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
 # Função para autenticar e obter o serviço do Google Drive
 def authenticate_google_drive():
     """Verifica se já existe um serviço de autenticação com o Google Drive no session_state"""
