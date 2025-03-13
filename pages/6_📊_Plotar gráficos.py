0d11995325b6e4f40b8ae141e39f3518ef67bc1d
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-13 01:08:51 -0400

    Update 6_📊_Plotar gráficos.py

--- pages/6_📊_Plotar gráficos.py
+++ pages/6_📊_Plotar gráficos.py
@@ -350,7 +350,7 @@ st.title("Gráficos - peso e consumo de ração")
 
 with st.container(border=True):
     with st.expander("Clique para mais informações sobre o aplicativo"):
-        st.write()
+        st.write("A planilha deve estar no formato do gerador")
 
 with st.container(border=True):
     st.write()
