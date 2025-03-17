603de3d3836e20aae783e4383decc31af0dbe2c3
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-17 00:49:19 -0400

    Update 3__📊_Plotar gráficos.py

--- pages/3__📊_Plotar gráficos.py
+++ pages/3__📊_Plotar gráficos.py
@@ -380,7 +380,7 @@ def plotar_pesagem_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro
     #y_scale = alt.Scale(domain=[min_peso - 1, max_peso + 1])
     
     # Ajustando a escala para o eixo Y
-    y_scale = alt.Scale(domain=[20, 40])#df['Peso'].min() - df['Erro'].max(), df['Peso'].max() + df['Erro'].max()])
+    y_scale = alt.Scale(domain=[20, 40], nice=False)#df['Peso'].min() - df['Erro'].max(), df['Peso'].max() + df['Erro'].max()])
 
     # Criação do gráfico de erro
     error_chart = alt.Chart(df).mark_errorband(extent='stderr').encode(
