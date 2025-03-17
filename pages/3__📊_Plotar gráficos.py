b77a20b2a849a24d057c64ae9aff0f611eab4c7c
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-17 00:31:09 -0400

    Update 3__📊_Plotar gráficos.py

--- pages/3__📊_Plotar gráficos.py
+++ pages/3__📊_Plotar gráficos.py
@@ -448,7 +448,7 @@ def plotar_pesagem_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro
     #st.altair_chart(chart, use_container_width=True)
 
 # Função para plotar o gráfico de área sombreada usando Altair
-#def plotar_pesagem_area_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_padrao_peso_dt):
+"""def plotar_pesagem_area_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro_padrao_peso_dt):
     dias = np.arange(1, len(medias_peso_ct) + 1)
 
     # Criando DataFrame para CT (Controle)
@@ -536,7 +536,7 @@ def plotar_pesagem_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro
         titleFontSize=14
     )
 
-    st.altair_chart(grafico_final)
+    st.altair_chart(grafico_final)"""
 
 
 # Dicionário de funções
