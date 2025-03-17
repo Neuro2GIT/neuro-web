8e71b9785fc5032f56a8fefa21773c41e13ab707
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-17 10:25:33 -0500

    Update 2_📜_Procedimentos.py

--- pages/2_📜_Procedimentos.py
+++ pages/2_📜_Procedimentos.py
@@ -88,7 +88,7 @@ def main():
     # Criar um container para a técnica selecionada com três expanders
     if tecnica_selecionada == "Preparo de ração":
 
-        with st.container(border=True, background_color="#b4fff2"):
+        with st.container(border=True):
             st.title("Preparo de ração")
             # Expander para "Ingredientes ração"
             with st.expander("Ingredientes ração"):
