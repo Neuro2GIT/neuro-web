e05370ecdb3350c34e9c1536368c8d4b4bf4206b
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-17 02:08:23 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -89,15 +89,10 @@ def main():
 
     # Exemplo de conteúdo estático para adicionar expanders
     static_themes = {
-        "Tema 1: Introdução à Neurociência": {
-            "Resumo": "Este é um resumo introdutório sobre neurociência.",
+        "Modelo de Deficiência de Tiamina": {
+            "Resumo": "Alterações cognitivas espaciais e parâmetros neuroquímicos cerebrais associados aos processos de morte celular em modelos experimentais de deficiência de tiamina e/ou consumo de etanol.",
             "Detalhes": "Detalhes sobre as várias áreas da neurociência, incluindo estrutura cerebral, neuroplasticidade, etc.",
             "Referências": "Aqui estão algumas referências importantes para estudar neurociência."
-        },
-        "Tema 2: Neurociência Cognitiva": {
-            "Resumo": "Este tema explora os fundamentos da neurociência cognitiva.",
-            "Detalhes": "Aqui discutimos como o cérebro processa informações, memória, linguagem e percepção.",
-            "Referências": "Referências sobre neurociência cognitiva e suas descobertas mais recentes."
         }
     }
 
