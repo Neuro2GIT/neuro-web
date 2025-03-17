56d439ef42d71931c33584c946841bf2931d2209
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-16 23:04:29 -0400

    Update 3__📊_Plotar gráficos.py

--- pages/3__📊_Plotar gráficos.py
+++ pages/3__📊_Plotar gráficos.py
@@ -390,7 +390,7 @@ def plotar_pesagem_alt(medias_peso_ct, erro_padrao_peso_ct, medias_peso_dt, erro
         y2='Peso + Erro:Q',  # Valor final da barra de erro (Peso + Erro)
         #y2='Peso:Q',  # Início da barra de erro
         color='Grupo:N',  # Cor por grupo
-        size=alt.value(4)  # Tamanho da barra de erro
+        size=alt.value(10)  # Tamanho da barra de erro
     #).transform_calculate(
         #y2='datum.Peso + datum.Erro'  # A barra de erro vai de Peso até Peso + Erro
     )
