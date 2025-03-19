bee0ead79b5b48db68e53518672f7477a67b5868
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-19 17:20:41 -0400

    Update 6__📊_Plotar gráficos.py

--- pages/6__📊_Plotar gráficos.py
+++ pages/6__📊_Plotar gráficos.py
@@ -373,8 +373,7 @@ plot_funcs = {
 st.title("Peso e consumo de ração")
 st.write("Para utilizar, converta a sua planilha para o modelo ou gere uma nova no gerador de planilhas")
 
-with st.container(border=True):
-    uploaded_file = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"])
+uploaded_file = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"])
 
 if uploaded_file is not None:
     # Processa os dados
