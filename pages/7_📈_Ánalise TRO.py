df1ee2300727371efaef4f15f5608f3d8d5afa95
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-19 18:05:38 -0400

    Update 7_📈_Ánalise TRO.py

--- pages/7_📈_Ánalise TRO.py
+++ pages/7_📈_Ánalise TRO.py
@@ -38,5 +38,5 @@ if uploaded_file is not None:
     # Exibir os resultados
     for sheet_name, df in resultados.items():
         with st.expander(f"### {sheet_name}"):
-            st.write(f"### {sheet_name}")
+            #st.write(f"### {sheet_name}")
             st.dataframe(df[["ID do animal", "Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência"]])
