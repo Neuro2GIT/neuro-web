71dab356c21271622a7620dcbc852c34b362769b
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-19 17:19:04 -0400

    Update 6__📊_Plotar gráficos.py

--- pages/6__📊_Plotar gráficos.py
+++ pages/6__📊_Plotar gráficos.py
@@ -376,6 +376,7 @@ with st.expander("Como usar?"):
     st.write("Converta a sua planilha para o modelo ou gere uma nova no gerador de planilhas")
 
 with st.container(border=True):
+    st.write("Converta a sua planilha para o modelo ou gere uma nova no gerador de planilhas")
     uploaded_file = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"])
 
 if uploaded_file is not None:
