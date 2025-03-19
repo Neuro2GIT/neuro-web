655ef6915a6861a0f26497101fc042c48a4e456f
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-19 16:44:20 -0400

    Update 6__📊_Plotar gráficos.py

--- pages/6__📊_Plotar gráficos.py
+++ pages/6__📊_Plotar gráficos.py
@@ -435,7 +435,7 @@ if uploaded_file is not None:
     })
 
     # Exibir os dados em formato de tabela
-    with st.expander():
+    with st.expander(f"### {sheet_name}"):
         st.write("### Média geral do consumo de ração")
         st.dataframe(df_medias_gerais)
 
