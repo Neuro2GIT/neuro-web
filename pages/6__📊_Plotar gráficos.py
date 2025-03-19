f314edc2424152aa483392eb52f3616b8d102087
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-19 17:59:32 -0400

    Update 6__📊_Plotar gráficos.py

--- pages/6__📊_Plotar gráficos.py
+++ pages/6__📊_Plotar gráficos.py
@@ -399,6 +399,8 @@ if uploaded_file is not None:
     # Seleciona as funções de acordo com a biblioteca escolhida
     plotar_funcoes = plot_funcs[aba_selecionada]
 
+    st.markdown("---")
+    
     st.subheader(f"{aba_selecionada}")
 
     # Exibe os gráficos correspondentes
