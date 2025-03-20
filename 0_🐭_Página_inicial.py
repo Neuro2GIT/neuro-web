2af9099d3dd80d61d972cd40a54898492b49b658
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 09:49:16 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -89,8 +89,9 @@ def main():
     
     st.markdown("<br>", unsafe_allow_html=True)
 
-    # Exemplo de conteúdo estático para adicionar expanders
-    static_themes = {
+    # Conteúdo estático para adicionar a dissertação
+    left.markdown(
+        static_themes = {
         "Modelo de Deficiência de Tiamina": {
             "Alterações cognitivas espaciais e parâmetros neuroquímicos cerebrais associados aos processos de morte celular em modelos experimentais de deficiência de tiamina e/ou consumo de etanol.":"",
             "Autor": "Rogério de Freitas Lacerda",
@@ -98,7 +99,7 @@ def main():
             "Repositorio UFMG": "http://hdl.handle.net/1843/33624",
             "Baixar PDF": "https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf"
         }
-    }
+    })
 
     # Exibindo os temas estáticos em expanders
     for theme, content in static_themes.items():
