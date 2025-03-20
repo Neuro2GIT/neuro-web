a2225c20726559b29433204f15e278a2013503bc
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 09:44:09 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -111,7 +111,7 @@ def main():
     for theme, dois in themes.items():
         with st.container(border=True):
             st.subheader(theme)  # Exibir o nome do tema como um subtítulo
-            st.markdown("---")  # Linha separadora para melhor organização
+            #st.markdown("---")  # Linha separadora para melhor organização
 
             for doi in dois:
                 title, authors, published_year, url, pdf_link = get_doi_info(doi)
