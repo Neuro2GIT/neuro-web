ecd8d125ff414c0ee8b037b6a71f9012de20ab7c
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 09:36:51 -0400

    Update 5_📝_Gerador de planilhas 2.py

--- pages/5_📝_Gerador de planilhas 2.py
+++ pages/5_📝_Gerador de planilhas 2.py
@@ -55,6 +55,8 @@ def criar_planilha(num_animais_ct, num_animais_dt):
 # Configuração do Streamlit
 st.title("Planilha para o TRO")
 
+st.markdown("---")
+
 with st.expander("Como funciona?"):
     st.write("O gerador criará um arquivo excel contendo duas planilhas, uma para inserir os tempos de exploração para os animais CT e a outra os animais DT.")
 
