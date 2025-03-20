c47a2d81baede0d84722d78b6ffc0fa0d6d6e1dd
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 09:58:33 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -84,51 +84,47 @@ themes = {
 # Função principal para exibir o conteúdo
 def main():
     st.title("🧠 Neuroscience Interest Group")
-
-    col1, col2 = st.columns(2)
     
     st.markdown("---")
     
     st.markdown("<br>", unsafe_allow_html=True)
 
     # Conteúdo estático para adicionar a dissertação
-    with col1:
-        static_themes = {
-            "Modelo de Deficiência de Tiamina": {
-                "Alterações cognitivas espaciais e parâmetros neuroquímicos cerebrais associados aos processos de morte celular em modelos experimentais de deficiência de tiamina e/ou consumo de etanol.":"",
-                "Autor": "Rogério de Freitas Lacerda",
-                "Tese de doutorado": "2020",
-                "Repositorio UFMG": "http://hdl.handle.net/1843/33624",
-                "Baixar PDF": "https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf"
-            }
+    static_themes = {
+        "Modelo de Deficiência de Tiamina": {
+            "Alterações cognitivas espaciais e parâmetros neuroquímicos cerebrais associados aos processos de morte celular em modelos experimentais de deficiência de tiamina e/ou consumo de etanol.":"",
+            "Autor": "Rogério de Freitas Lacerda",
+            "Tese de doutorado": "2020",
+            "Repositorio UFMG": "http://hdl.handle.net/1843/33624",
+            "Baixar PDF": "https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf"
         }
+    }
 
-    with col2:
-        # Exibindo os temas estáticos em expanders
-        for theme, content in static_themes.items():
-            with st.expander(theme):
-                for section_title, section_content in content.items():
-                    st.markdown(f"**{section_title}**")
-                    st.markdown(section_content)
+    # Exibindo os temas estáticos em expanders
+    for theme, content in static_themes.items():
+        with st.expander(theme):
+            for section_title, section_content in content.items():
+                st.markdown(f"**{section_title}**")
+                st.markdown(section_content)
     
-        # Iterar pelos temas e artigos
-        for theme, dois in themes.items():
-            with st.container(border=True):
-                st.subheader(theme)  # Exibir o nome do tema como um subtítulo
-                #st.markdown("---")  # Linha separadora para melhor organização
+    # Iterar pelos temas e artigos
+    for theme, dois in themes.items():
+        with st.container(border=True):
+            st.subheader(theme)  # Exibir o nome do tema como um subtítulo
+            #st.markdown("---")  # Linha separadora para melhor organização
 
-                for doi in dois:
-                    title, authors, published_year, url, pdf_link = get_doi_info(doi)
+            for doi in dois:
+                title, authors, published_year, url, pdf_link = get_doi_info(doi)
             
-                    if title:
-                        with st.expander(title):
-                             st.markdown(f"**Autores**: {authors}")
-                             st.markdown(f"**Publicado em**: {published_year}")
-                             st.markdown(f"[Leia o artigo completo]({url})")
+                if title:
+                    with st.expander(title):
+                        st.markdown(f"**Autores**: {authors}")
+                        st.markdown(f"**Publicado em**: {published_year}")
+                        st.markdown(f"[Leia o artigo completo]({url})")
                     
-                             # Botão para baixar o PDF, se disponível
-                             if pdf_link:
-                                 st.markdown(f"[Baixar PDF]({pdf_link})")
+                        # Botão para baixar o PDF, se disponível
+                        if pdf_link:
+                            st.markdown(f"[Baixar PDF]({pdf_link})")
 
                          #LINK DA TESE DO PROFESSOR: http://hdl.handle.net/1843/33624 PDF: https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf
                     
