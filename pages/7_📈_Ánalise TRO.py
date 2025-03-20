2b57d5109aaae267f985917427ff81068bebb188
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 15:20:11 -0500

    Update 7_📈_Ánalise TRO.py

--- pages/7_📈_Ánalise TRO.py
+++ pages/7_📈_Ánalise TRO.py
@@ -24,11 +24,11 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     with st.container(border=True):
-        uploaded_file1 = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"])
+        uploaded_file1 = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"], key="file_uploader_1")
 
 with col1:
     with st.container(border=True):
-        uploaded_file2 = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"])
+        uploaded_file2 = st.file_uploader("Selecione um arquivo excel (.xlsx)", type=["xlsx"], key="file_uploader_2")
 
 
 
