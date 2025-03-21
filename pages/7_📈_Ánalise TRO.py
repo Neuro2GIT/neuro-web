2e53d05d4458ddbc6b55ad17953e954d914e747b
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 22:23:47 -0400

    Update 7_📈_Ánalise TRO.py

--- pages/7_📈_Ánalise TRO.py
+++ pages/7_📈_Ánalise TRO.py
@@ -48,8 +48,8 @@ if uploaded_file is not None:
     for sheet_name, df in resultados.items():
         with st.expander(f"### {sheet_name}"):
             #st.write(f"### {sheet_name}")
-            df.set_index("ID do animal", inplace=True)
             st.dataframe(df[["ID do animal", "Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta"]])
+            df.set_index("ID do animal", inplace=True)
 
     # Criar um arquivo Excel com os resultados
     with pd.ExcelWriter("resultados_TRO.xlsx", engine="xlsxwriter") as writer:
