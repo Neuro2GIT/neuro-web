1e55a626084645434e77da66368637b132f4b057
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 22:30:43 -0400

    Update 7_📈_Ánalise TRO.py

--- pages/7_📈_Ánalise TRO.py
+++ pages/7_📈_Ánalise TRO.py
@@ -47,13 +47,9 @@ if uploaded_file is not None:
     # Exibir os resultados
     for sheet_name, df in resultados.items():
         with st.expander(f"### {sheet_name}"):
-            #st.write(f"### {sheet_name}")
-            # Resetando o índice e removendo a coluna de índice
-            df_resetado = df[["ID do animal", "Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar", 
-                              "Índice de discriminação", "Índice de preferência", "Discriminação absoluta"]].reset_index(drop=True)
-            st.dataframe(df_resetado)
-            #st.dataframe(df[["ID do animal", "Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta"]])
-            #df.set_index("ID do animal", inplace=True)
+            #st.write(f"### {sheet_name}")           
+            st.dataframe(df[["ID do animal", "Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta"]])
+            df.set_index("ID do animal", inplace=True)
             
 
     # Criar um arquivo Excel com os resultados
