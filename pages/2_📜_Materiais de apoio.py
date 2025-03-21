a0560fbfe31c37d53f78101d20642ea46b83ae10
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-20 23:34:04 -0400

    Update 2_📜_Materiais de apoio.py

--- pages/2_📜_Materiais de apoio.py
+++ pages/2_📜_Materiais de apoio.py
@@ -90,7 +90,7 @@ def main():
         
         st.title("Preparo de ração")
         url = "https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf"
-        st.markdown("Fonte f"[dissertação de doutorado]({url})")
+        st.markdown("Fonte f"[dissertação de doutorado]({url})"")
 
         st.markdown("---")
         
