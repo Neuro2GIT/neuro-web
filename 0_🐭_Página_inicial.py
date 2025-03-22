92af141ba60a15620d81095ed3573c23198f2b3f
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-22 10:04:15 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -106,14 +106,13 @@ def main():
             for section_title, section_content in content.items():
                 st.markdown(f"**{section_title}**")
                 st.markdown(section_content)
+        st.write("")
     
     # Iterar pelos temas e artigos
     for theme, dois in themes.items():
         with st.container(border=True):
             st.subheader(theme)  # Exibir o nome do tema como um subtítulo
             #st.markdown("---")  # Linha separadora para melhor organização
-        
-        st.write("")
             
         for doi in dois:
             title, authors, published_year, url, pdf_link = get_doi_info(doi)
