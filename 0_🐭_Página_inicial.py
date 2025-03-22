d16855fec92d4198fc247d7eb30e454e8b3995fa
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-22 10:06:04 -0400

    Update 0_🐭_Página_inicial.py

--- 0_🐭_Página_inicial.py
+++ 0_🐭_Página_inicial.py
@@ -127,7 +127,7 @@ def main():
                         if pdf_link:
                             st.markdown(f"[Baixar PDF]({pdf_link})")
 
-            st.markdown("<br><br>", unsafe_allow_html=True)
+        st.markdown("<br><br>", unsafe_allow_html=True)
 
                          #LINK DA TESE DO PROFESSOR: http://hdl.handle.net/1843/33624 PDF: https://repositorio.ufmg.br/bitstream/1843/33624/1/Tese%20vers%c3%a3o%20FINAL.pdf
                     
