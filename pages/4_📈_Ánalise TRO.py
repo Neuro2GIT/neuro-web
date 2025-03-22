8d258bf72a500b14d6c53d5db1ca96c4ff471a69
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-22 14:13:47 -0400

    Update 4_📈_Ánalise TRO.py

--- pages/4_📈_Ánalise TRO.py
+++ pages/4_📈_Ánalise TRO.py
@@ -2,6 +2,14 @@ import pandas as pd
 import streamlit as st
 
 
+st.set_page_config(
+    page_title="Análise do TRO",
+    page_icon="🐭",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={})
+st.set_option('client.showErrorDetails', True)
+
 def calcular_indices(df):
     # Calcula os índices de discriminação e de preferencia para cada animal.
     df["Discriminação absoluta"] = ((df["Tempo no objeto novo"] - df["Tempo no objeto familiar"]))
