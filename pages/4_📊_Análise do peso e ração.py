06349f361fe658b03171d1a344963972b31f159c
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-23 18:42:04 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -515,6 +515,14 @@ if uploaded_file is not None:
 
         # Dataframe com a media geral do cosumo de ração
         with st.container(border=True):
+            # Criar DataFrame para consumo geral de ração
+            df_medias_gerais = pd.DataFrame({
+                "Grupo": ["CT", "DT"],
+                "Média do consumo de ração": [
+                    resultados_racao["media_geral_racao_ct"],  # Usando a variável 'dados' para acessar a média
+                    resultados_racao["media_geral_racao_dt"]   # Usando a variável 'dados' para acessar a média
+                ]
+            })
             st.write("Média geral de consumo de ração por grupo")
             df_medias_gerais.set_index("Grupo", inplace=True)
             st.dataframe(df_medias_gerais)
