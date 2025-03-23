f2d73538d880636e0ae823584a619aec8c69ac52
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-23 18:44:53 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -506,9 +506,7 @@ if uploaded_file is not None:
         # Dataframe com o erro padrão
         with st.container(border=True):
             df_errpadrao = pd.DataFrame({
-                "Grupo": ["CT", "DT"],
-                dados["erro_padrao_peso_ct"],
-                dados["erro_padrao_peso_dt"]
+                "Grupo": ["CT"dados["erro_padrao_peso_ct"], "DT"dados["erro_padrao_peso_dt"]]
             })
             df_errpadrao = df_errpadrao.T # Transpor para que as métricas fiquem nas linhas e os dias/observações nas colunas
             st.write("Erro padrão")
