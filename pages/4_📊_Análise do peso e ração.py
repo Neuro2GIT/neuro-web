0d1c204f5662eea061fad79b0149266152f4eab8
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-23 18:29:45 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -454,17 +454,19 @@ if uploaded_file is not None:
 
     # Cria um botão para download do arquivo Excel gerado
     with open(arquivo, "rb") as file:
-        st.download_button(
+        #st.download_button(
             label="Fazer download - dados processados",
             data=file,
             file_name=arquivo,
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
-    
+
+st.write("Selecione abaixo para navegar entre as opções")
+
     with st.container(border=True):
         # Cria as abas usando st.radio
         aba_selecionada = st.radio(
-            "Escolha a biblioteca",
+            "Escolha uma biblioteca",
             ("Plotly", "Matplotlib", "Seaborn")
         )
 
