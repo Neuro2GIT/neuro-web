39d36205a69303b4e10d2d294122f55c34d05af0
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-23 14:57:56 -0400

    Update new3.py

--- pages/new3.py
+++ pages/new3.py
@@ -51,10 +51,10 @@ def processar_consumo_racao(uploaded_file):
     # Dicionário com os resultados do consumo
     return {
         "medias_racao_ct": medias_racao_ct,
-        "medias_racao_dt": medias_racao_dt,
         "media_geral_racao_ct": media_geral_racao_ct,
-        "media_geral_racao_dt": media_geral_racao_dt,
         "df_racao_ct": df_racao_ct, 
+        "medias_racao_dt": medias_racao_dt,
+        "media_geral_racao_dt": media_geral_racao_dt,
         "df_racao_dt": df_racao_dt
     }
 
