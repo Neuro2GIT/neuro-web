83d1ccf7198b65fcdd320de64fb547d01aac4685
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-23 14:21:15 -0400

    Update new3.py

--- pages/new3.py
+++ pages/new3.py
@@ -88,7 +88,10 @@ if uploaded_file:
 
     st.write("Média de consumo de ração para classe DT:")
     st.dataframe(resultado_racao["df_racao_dt"])
-
+    
     # Exibir resumo das médias gerais
     st.subheader("Médias Gerais de Consumo de Ração")
-    st.dataframe(f"Média geral de ração para CT: {resultado_racao['media_geral_racao_ct']}")
+    df_media_geral = pd.concat([resultado_racao["media_geral_racao_ct"], resultado_racao["media_geral_racao_dt"]])
+    df_media_geral.set_index("Classe da Caixa", implace=True)
+    st.dataframe(df_media_geral)
+    #st.dataframe(f"Média geral de ração para CT: {resultado_racao['media_geral_racao_ct']}")
