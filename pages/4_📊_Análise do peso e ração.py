a1d0c6aec0069088c812ca7cbf93f94c296a462b
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-25 16:55:26 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -52,23 +52,6 @@ def carregar_e_processar_excel(uploaded_file):
         "medias_peso_dt": medias_peso_dt, "erro_padrao_peso_dt": erro_padrao_peso_dt, "df_dt": df_dt,
     }
 
-    # Criando o DataFrame com os dados
-    df = pd.DataFrame({
-        'Dias': dias * 2,  # Replicando os dias para cada grupo
-        'Peso (g)': medias_peso_ct + medias_peso_dt,  # Concatenando as médias
-        'Grupo': ['Controle'] * len(dias) + ['Deficiente em tiamina'] * len(dias)  # Criando as categorias
-    })
-
-    # Criando o gráfico de linhas com Altair
-    chart = alt.Chart(df).mark_line().encode(
-        x='Dias:O',  # Eixo X como ordinal (dias)
-        y='Peso (g):Q',  # Eixo Y como quantitativo (peso)
-        color='Grupo:N',  # Colorir por grupo
-        detail='Grupo:N'  # Detalhes para o gráfico de linhas, separando por grupo
-    ).properties(
-        title="Peso médio dos animais por dia de experimento"
-    )
-
     # Criar o DataFrame com as médias para cada dia em cada classe
     #df_resultado = pd.DataFrame({
         #"Média Peso CT": medias_peso_ct,
@@ -544,6 +527,23 @@ if uploaded_file is not None:
             df_medias_gerais.set_index("Grupo", inplace=True)
             st.dataframe(df_medias_gerais)
 
+        # Criando o DataFrame com os dados
+        df = pd.DataFrame({
+            'Dias': dias * 2,  # Replicando os dias para cada grupo
+            'Peso (g)': medias_peso_ct + medias_peso_dt,  # Concatenando as médias
+            'Grupo': ['Controle'] * len(dias) + ['Deficiente em tiamina'] * len(dias)  # Criando as categorias
+        })
+
+        # Criando o gráfico de linhas com Altair
+        chart = alt.Chart(df).mark_line().encode(
+            x='Dias:O',  # Eixo X como ordinal (dias)
+            y='Peso (g):Q',  # Eixo Y como quantitativo (peso)
+            color='Grupo:N',  # Colorir por grupo
+            detail='Grupo:N'  # Detalhes para o gráfico de linhas, separando por grupo
+        ).properties(
+            title="Peso médio dos animais por dia de experimento"
+        )
+    
         st.altair_chart(chart, use_container_width=True)
 
     # Exibir os valores de forma destacada
