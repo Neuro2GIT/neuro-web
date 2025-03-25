f12a9536ece52232f0c89ff9dfa06cad9b17bf1a
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-25 17:21:00 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -518,7 +518,7 @@ if uploaded_file is not None:
         # Cria as abas usando st.radio
         aba_selecionada = st.radio(
             "Selecione uma biblioteca abaixo para navegar entre as opções",
-            ("Plotly", "Matplotlib", "Seaborn", "Altair")
+            ("Plotly", "Matplotlib", "Seaborn",)
         )
 
     # Seleciona as funções de acordo com a biblioteca escolhida
