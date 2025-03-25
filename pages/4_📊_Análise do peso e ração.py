bf115b45a2fc4ebf8f889f98d7ad9a9d56f8775e
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-24 20:06:04 -0400

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -533,7 +533,7 @@ if uploaded_file is not None:
     
 
     # Dados brutos de peso e consumo de ração em tabelas
-    with st.expander("Tabelas com os dados brutos: médias, erro padrão e consumo de ração"):
+    with st.expander("Tabelas com os dados brutos: peso e consumo de ração"):
         with st.container(border=True):
             st.write("Peso dos animais")
             df_peso_animais = pd.concat([dados["df_ct"], dados["df_dt"]])
