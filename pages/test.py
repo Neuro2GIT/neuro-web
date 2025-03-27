aad24533a43eb8a679feb782d861829ca946c3d4
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 15:25:16 -0400

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -36,25 +36,21 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file, sheet_name=None)
 
     # Verifica se as colunas necessárias existem no arquivo
-    colunas_necessarias = {"Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar"}
+    #colunas_necessarias = {"Classe do animal", "Tempo no objeto novo", "Tempo no objeto familiar"}
     
-    if colunas_necessarias.issubset(df.columns):
-        # Calcular índices e médias
-        df = calcular_indices(df)
-        medias_por_classe = calcular_medias(df)
+    df = calcular_indices(df)
+    medias_por_classe = calcular_medias(df)
 
-        # Armazenando os resultados organizados
-        resultados = {
-            "dados_com_indices": df,
-            "medias_por_classe": medias_por_classe
-        }
+    # Armazenando os resultados organizados
+    resultados = {
+        "dados_com_indices": df,
+        "medias_por_classe": medias_por_classe
+    }
 
-        # Exibir tabelas no Streamlit
-        st.subheader("Dados com Índices Calculados")
-        df.set_index("Classe do animal", inplace=True)
-        st.dataframe(resultados["dados_com_indices"])
+    # Exibir tabelas no Streamlit
+    st.subheader("Dados com Índices Calculados")
+    df.set_index("Classe do animal", inplace=True)
+    st.dataframe(resultados["dados_com_indices"])
 
-        st.subheader("Médias dos Índices por Classe")
-        st.dataframe(resultados["medias_por_classe"])
-    else:
-        st.error(f"O arquivo deve conter as colunas: {', '.join(colunas_necessarias)}")
+    st.subheader("Médias dos Índices por Classe")
+    st.dataframe(resultados["medias_por_classe"])
