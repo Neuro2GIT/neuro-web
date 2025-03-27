45f8e565a86b66c6003827e620d14911f8ef793e
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 15:17:40 -0400

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -51,7 +51,7 @@ if uploaded_file is not None:
 
         # Exibir tabelas no Streamlit
         st.subheader("Dados com Índices Calculados")
-        df.set_index("Classe", inplace=True)
+        df.set_index("Classe do animal", inplace=True)
         st.dataframe(resultados["dados_com_indices"])
 
         st.subheader("Médias dos Índices por Classe")
