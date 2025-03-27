21f1ebbf540d1b0d60e368473cb3e7c4fccaf798
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 14:53:31 -0400

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -49,9 +49,4 @@ if uploaded_file is not None:
     # Criar um dicionário para armazenar os índices
     resultados = {}
 
-    # Exibir dados de cada planilha
-    for sheet in sheet_names:
-        st.write(f"Exibindo dados da planilha: {sheet}")
-        df = dados[sheet]
-
     
