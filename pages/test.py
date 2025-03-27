d58573b7871b5deabd8432ebd215b2ee73b4afc4
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 14:50:24 -0400

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -47,43 +47,4 @@ if uploaded_file is not None:
     # Criar um dicionário para armazenar os índices
     resultados = {}
 
-    # Iterar sobre as planilhas (Animais CT e Animais DT)
-    for sheet_name in dados.sheet_names:
-        df = pd.read_excel(dados, sheet_name=sheet_name)
-        df, medias_por_classe = calcular_indices(df)
-        resultados[sheet_name] = {"df": df, "medias_por_classe": medias_por_classe}
-
-    # Exibir os resultados
-    for sheet_name, result in resultados.items():
-        df = result["df"]
-        medias_por_classe = result["medias_por_classe"]
-        
-        # Exibir os dados de cada planilha
-        with st.expander(f"### {sheet_name}"):
-            df.set_index("Classe do animal", inplace=True)
-            st.dataframe(df[["Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta"]])
-
-        # Exibir as médias por classe de animal
-        with st.expander(f"Médias por Classe de Animal - {sheet_name}"):
-            st.dataframe(medias_por_classe)
-
-            # Exibir as médias de discriminação por classe
-            for _, row in medias_por_classe.iterrows():
-                st.write(f"Média do índice de discriminação para a classe {row['Classe do animal']}: {row['Índice de discriminação']:.2f}")
-
-            # Exibir gráficos por classe de animal
-            st.subheader(f"Gráfico de Índice de Discriminação para {sheet_name}")
-            st.bar_chart(medias_por_classe.set_index('Classe do animal')['Índice de discriminação'])
-            
-    # Exibir os resultados
-    #for sheet_name, df in resultados.items():
-        #with st.expander(f"### {sheet_name}"):
-            #df.set_index("Classe do animal", inplace=True)
-            #st.write(f"### {sheet_name}")           
-            #st.dataframe(df[["Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta", "ID"]])
-
-
-    with st.expander("Médias do índice de discriminação"):
-        # Para cada planilha, exibe a média do índice de discriminação
-        for sheet_name, df in resultados.items():
-            st.write(f"Média do índice de discriminação para {sheet_name}: {medias_por_classe}")
+    
