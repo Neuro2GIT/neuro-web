574800c64e441ccee4cb9abc114cb7767d27e031
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 14:07:53 -0400

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -64,7 +64,7 @@ if uploaded_file is not None:
         with st.expander(f"### {sheet_name}"):
             df.set_index("Classe do animal", inplace=True)
             #st.write(f"### {sheet_name}")           
-            st.dataframe(df_ct_m[["Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta", "ID"]])
+            st.dataframe(df)
 
 
     with st.expander("Médias do índice de discriminação"):
