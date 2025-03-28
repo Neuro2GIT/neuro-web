786df31ed21127795274c0ad5c3643ea8b83e7f9
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-28 10:23:58 -0500

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -616,7 +616,7 @@ if uploaded_file is not None:
                 #"Médias de consumo de ração": [
                     #resultados_racao["medias_racao_ct"],  # Usando a variável 'dados' para acessar a média
                     #resultados_racao["medias_racao_dt"]   # Usando a variável 'dados' para acessar a média
-                #]})
+                ]})
             st.write("Médias de consumo de ração por grupo")
             df_medias_gerais.set_index("Grupo", inplace=True)
             st.dataframe(df_medias_gerais)
