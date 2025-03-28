02c79458905ab7533460749e52fc7c6dc20fd667
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 21:58:54 -0400

    Update 4_📈_Ánalise do TRO.py

--- pages/4_📈_Ánalise do TRO.py
+++ pages/4_📈_Ánalise do TRO.py
@@ -19,6 +19,16 @@ def calcular_indices(df):
                                       (df["Tempo no objeto novo"] + df["Tempo no objeto familiar"]))
     
     df["Índice de preferência"] = ((df["Tempo no objeto novo"]) / (df["Tempo no objeto novo"] + df ["Tempo no objeto familiar"])) * 100
+
+    # Garantir que a coluna "Classe do animal" e "Grupo" estão no formato correto
+    df["Classe do animal"] = df["Classe do animal"].astype(str).str.upper().str.strip()
+    df["Grupo"] = df["Grupo"].astype(str).str.upper().str.strip()
+
+    # Filtrar dados válidos antes do cálculo (evita problemas com valores nulos)
+    df_validos = df.dropna(subset=["Índice de discriminação", "Classe do animal", "Grupo"])
+
+     Calcular a média do índice de discriminação para cada combinação de Classe do animal e Grupo
+    media_discriminacao = df_validos.groupby(["Classe do animal", "Grupo"], as_index=False)["Índice de discriminação"].mean()
     
     return df
 
@@ -76,8 +86,8 @@ if uploaded_file is not None:
     #media_discriminacao = df_validos.groupby(["Classe do animal", "Grupo"], as_index=False)["Índice de discriminação"].mean()
 
     # Exibir os resultados no Streamlit
-    #st.markdown("### Médias do Índice de Discriminação por Sexo e Grupo")
-    #st.dataframe(media_discriminacao.style.format({"Índice de discriminação": "{:.2f}"}))
+    st.markdown("### Médias do Índice de Discriminação por Sexo e Grupo")
+    st.dataframe(media_discriminacao.style.format({"Índice de discriminação": "{:.2f}"}))
 
 
     output = BytesIO()
