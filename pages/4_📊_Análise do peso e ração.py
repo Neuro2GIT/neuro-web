6e2ba2a462dc37d60ea7b7fe78b31d6b3cf215f3
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-28 09:52:29 -0500

    Update 4_📊_Análise do peso e ração.py

--- pages/4_📊_Análise do peso e ração.py
+++ pages/4_📊_Análise do peso e ração.py
@@ -630,7 +630,7 @@ if uploaded_file is not None:
 
         with st.container(border=True):
             st.write("Médias do consumo de ração")
-            df_media_racao = pd.concat([racao_processada["df_ct"], racao_processada["df_dt"]])
+            df_media_racao = pd.concat([racao_processada["df_racao_ct"], racao_processada["df_racao_ct"]])
             df_media_racao.set_index("Classe da Caixa", inplace=True)
             st.write(df_media_racao)
     
