737f04b2d822525836ed1ee1abda1dbd6dfed03d
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 20:16:33 -0400

    Update test3.py

--- pages/test3.py
+++ pages/test3.py
@@ -19,29 +19,17 @@ def calcular_indices(df):
     df_f_ct = separar_grupo_subgrupo(df, 'CT', 'F')  # Fêmeas - Controle
     df_f_dt = separar_grupo_subgrupo(df, 'DT', 'F')  # Fêmeas - Tratamento
     
-    # Calcular discriminação absoluta para os subgrupos
-    df_m_ct["Discriminação absoluta"] = df_m_ct["Tempo no objeto novo"] - df_m_ct["Tempo no objeto familiar"]
-    df_m_dt["Discriminação absoluta"] = df_m_dt["Tempo no objeto novo"] - df_m_dt["Tempo no objeto familiar"]
-    df_f_ct["Discriminação absoluta"] = df_f_ct["Tempo no objeto novo"] - df_f_ct["Tempo no objeto familiar"]
-    df_f_dt["Discriminação absoluta"] = df_f_dt["Tempo no objeto novo"] - df_f_dt["Tempo no objeto familiar"]
-    
     # Calcular Índice de Discriminação para M-CT, M-DT, F-CT, F-DT
-    df_m_ct["Índice de discriminação"] = df_m_ct["Discriminação absoluta"] / (df_m_ct["Tempo no objeto novo"] + df_m_ct["Tempo no objeto familiar"])
-    df_m_dt["Índice de discriminação"] = df_m_dt["Discriminação absoluta"] / (df_m_dt["Tempo no objeto novo"] + df_m_dt["Tempo no objeto familiar"])
-    df_f_ct["Índice de discriminação"] = df_f_ct["Discriminação absoluta"] / (df_f_ct["Tempo no objeto novo"] + df_f_ct["Tempo no objeto familiar"])
-    df_f_dt["Índice de discriminação"] = df_f_dt["Discriminação absoluta"] / (df_f_dt["Tempo no objeto novo"] + df_f_dt["Tempo no objeto familiar"])
-    
-    # Calcular Índice de Preferência para M-CT, M-DT, F-CT, F-DT
-    df_m_ct["Índice de preferência"] = (df_m_ct["Tempo no objeto novo"] / (df_m_ct["Tempo no objeto novo"] + df_m_ct["Tempo no objeto familiar"])) * 100
-    df_m_dt["Índice de preferência"] = (df_m_dt["Tempo no objeto novo"] / (df_m_dt["Tempo no objeto novo"] + df_m_dt["Tempo no objeto familiar"])) * 100
-    df_f_ct["Índice de preferência"] = (df_f_ct["Tempo no objeto novo"] / (df_f_ct["Tempo no objeto novo"] + df_f_ct["Tempo no objeto familiar"])) * 100
-    df_f_dt["Índice de preferência"] = (df_f_dt["Tempo no objeto novo"] / (df_f_dt["Tempo no objeto novo"] + df_f_dt["Tempo no objeto familiar"])) * 100
+    df_m_ct["Índice de discriminação"] = df_m_ct["Tempo no objeto novo"] - df_m_ct["Tempo no objeto familiar"] / (df_m_ct["Tempo no objeto novo"] + df_m_ct["Tempo no objeto familiar"])
+    df_m_dt["Índice de discriminação"] = df_m_dt["Tempo no objeto novo"] - df_m_ct["Tempo no objeto familiar"] / (df_m_dt["Tempo no objeto novo"] + df_m_dt["Tempo no objeto familiar"])
+    df_f_ct["Índice de discriminação"] = df_f_ct["Tempo no objeto novo"] - df_f_ct["Tempo no objeto familiar"] / (df_f_ct["Tempo no objeto novo"] + df_f_ct["Tempo no objeto familiar"])
+    df_f_dt["Índice de discriminação"] = df_f_dt["Tempo no objeto novo"] - df_f_dt["Tempo no objeto familiar"] / (df_f_dt["Tempo no objeto novo"] + df_f_dt["Tempo no objeto familiar"])
 
-    df_controle = pd.concat([df_m_ct, df_f_ct], ignore_index=True)
-    df_deficiencia = pd.concat([df_m_dt, df_f_dt], ignore_index=True)
+    df_controle = pd.concat([df_m_ct, df_f_ct])
+    df_deficiencia = pd.concat([df_m_dt, df_f_dt])
     
     # Retornar os dataframes
-    return df_controle, df_deficiencia, medias_indices
+    return
 
 # Streamlit: Interface do usuário
 st.title("Cálculo de Índices de Discriminação e Preferência")
@@ -54,7 +42,7 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
 
     # Calcular os índices
-    df_controle, df_deficiencia = calcular_indices(df)
+    df_controle, df_deficiencia, medias_indices = calcular_indices(df)
     
     # Exibir o DataFrame concatenado de Controle (M-CT e F-CT)
     st.subheader("Resultados de Machos e Fêmeas - Controle (M-CT e F-CT)")
