3d2a90e5ddd942ca087fea4f83ef5b553fd7acfc
Neuro2GIT <gruponeuroscience@gmail.com>
2025-03-27 21:51:45 -0400

    Update 4_📈_Ánalise do TRO.py

--- pages/4_📈_Ánalise do TRO.py
+++ pages/4_📈_Ánalise do TRO.py
@@ -61,14 +61,14 @@ if uploaded_file is not None:
             #st.write(f"### {sheet_name}")           
             st.dataframe(df[["Grupo", "Tempo no objeto novo", "Tempo no objeto familiar", "Índice de discriminação", "Índice de preferência", "Discriminação absoluta", "ID"]])
 
-    # Garantir que a coluna "Classe" e "Grupo" estão no formato correto
+    # Garantir que a coluna "Classe do animal" e "Grupo" estão no formato correto
     df["Classe do animal"] = df["Classe do animal"].astype(str).str.upper().str.strip()
     df["Grupo"] = df["Grupo"].astype(str).str.upper().str.strip()
 
     # Filtrar dados válidos antes do cálculo (evita problemas com valores nulos)
     df_validos = df.dropna(subset=["Índice de discriminação", "Classe do animal", "Grupo"])
 
-    # Calcular a média do índice de discriminação para cada combinação de Classe e Grupo
+    # Calcular a média do índice de discriminação para cada combinação de Classe do animal e Grupo
     media_discriminacao = df_validos.groupby(["Classe do animal", "Grupo"], as_index=False)["Índice de discriminação"].mean()
 
     # Exibir os resultados no Streamlit
